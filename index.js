bf990cc8d7c8967a19fdb9bd156f5171dfcbe461
ricardowoj <ricardowoj@hotmail.com>
2020-04-19 10:42:56 -0300

    Add middleware.

--- index.js
+++ index.js
@@ -10,55 +10,81 @@ server.use(express.json())
 
 //CRUD - Cread, Read, Update, Delete
 
-const apartamentProject = [
-    {
-        id: 1,
-        title: "Apartamento",
-        tasks: "ASsinatura de contrato"
+/**
+ * A variável `apartamentProjects` pode ser `const` porque um `array`
+ * pode receber adições ou exclusões mesmo sendo uma constante.
+ */
+
+const apartamentProjects = []
+
+/**
+ * Middleware que checa se o projeto existe
+ */
+function checkProjectExists(req, res, next) {
+
+    const { id } = req.params
+    const apartamentProject = apartamentProjects.find(p => p.id == (id + 1)) 
+
+    if (!apartamentProject) {
+        return res.status(400).json({ error: 'Project not found'})
     }
-]
 
-server.get('/apartamentProject', (req, res) => {
+    return next();
+}
 
-    return res.json(apartamentProject)
+/**
+ * Middleware que dá log no número de requisições
+ */
+function logRequests(req, res, next) {
+
+    console.count("Número de requisições");
+  
+    return next();
+  }
+  
+server.use(logRequests);
+
+server.get('/apartamentProjects', (req, res) => {
+
+    return res.json(apartamentProjects)
 })
-server.get('/apartamentProject/:id', (req, res) => {
+server.get('/apartamentProjects/:id', (req, res) => {
 
     const { id } = req.params
 
-    return res.json(
-        "Título do projeto: " + apartamentProject[id].title + " => Tarefa: " + apartamentProject[id].tasks
-    )
+    return res.json(apartamentProjects[id])
 })
-server.post('/apartamentProject', (req, res) => {
+server.post('/apartamentProjects', (req, res) => {
 
     const { title, tasks } = req.body
 
-    id = (apartamentProject.length + 1)
+    id = (apartamentProjects.length + 1)
 
-    apartamentProject.push({ id, title, tasks })
+    apartamentProjects.push({ id, title, tasks })
 
-    return res.json(apartamentProject)
+    return res.json(apartamentProjects)
 
 })
-server.put('/apartamentProject/:id', (req, res) => {
+server.put('/apartamentProjects/:id', checkProjectExists, (req, res) => {
 
     const { id } = req.params
     const { title, tasks } = req.body
 
-    apartamentProject[id].id = parseInt(id) + 1
-    apartamentProject[id].title = title 
-    apartamentProject[id].tasks = tasks
+    apartamentProjects[id].id = parseInt(id) + 1
+    apartamentProjects[id].title = title 
+    apartamentProjects[id].tasks = tasks
 
-    return res.json(apartamentProject[id]) 
+    return res.json(apartamentProjects[id]) 
 })
-server.delete('/userts/:id', (req, res) => {
+server.delete('/apartamentProjects/:id', checkProjectExists, (req, res) => {
 
     const { id } = req.params
 
-    apartamentProject.splice(id, 1)    
+    const apartamentProjectsIndex = apartamentProjects.findIndex(p => p.id == id)
+
+    apartamentProjects.splice(apartamentProjectsIndex, 1)    
     
-    return res.send()
+    return res.json({ delete: `Successfully removed ${id}` })
 })
 
 server.listen(3003) 
